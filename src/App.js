433ee52e664149214b964e54d216e2ab5ec521ea
yijungai <ethan.yijun@gmail.com>
2020-05-05 22:54:36 +1000

    add reducers and actions, link them to the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
+import {decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged?
+        <h3>Valuable Information I shouldn't see: </h3>
+      :<h3>You are not authorised to see :)</h3>}
     </div>
   );
 }
